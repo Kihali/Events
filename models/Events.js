16397489b67c916cd5d2ec678a3691c3cc0c9558
Allan-njoroge <allandavis254@gmail.com>
2024-05-23 21:54:34 +0300

    Change of the model schema

--- models/Events.js
+++ models/Events.js
@@ -7,7 +7,8 @@ const EventsSchema = new mongoose.Schema({
     image: { type: String, required: true },
     maxAttendees: { type: Number, required: true },
     price: { type: String, required: true },
-    location: { type: String, required: true },
+    link: {type: String },
+    // location: { type: String, required: true },
     date: { type: String, required: true },
     startTime: { type: String, required: true },
     endTime: { type: String, required: true }
