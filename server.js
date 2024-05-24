026588ef4c6f3299954c425b92d7cef09a164cbe
Allan-njoroge <allandavis254@gmail.com>
2024-05-24 20:29:19 +0300

    Updates

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(express.static(staticFilesDirectory));
 const verifyToken = (req, res, next) => {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
-
+    
     if (token == null) return res.sendStatus(403);
 
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
